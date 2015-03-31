26581fb32d909a29306f58abcaf690e2560268b8
Matej <matej.aleksandrov@gmail.com>
2015-03-31 08:42:32 +0200

    Bug fixes

--- GraphTopology.py
+++ GraphTopology.py
@@ -40,6 +40,19 @@ class TopologicalGraph():
                         self.edges[e1][3]=e
                         self.edges[e][2]=self.borders_num
                 self.borders_num+=1
+    
+    def __repr__(self):
+        s=str(len(self.vert_e))+' Vertices: \n'+str(self.vert_e)+'\n'
+        s+=str(len(self.edges)//2)+' Edges: \n'+str(self.edges)+'\n'
+        s+=str(len(self.faces))+' Faces: \n'+str(self.faces)+'\n'
+        s+=str(len(self.borders))+' Borders: \n'+str(self.borders)
+        #'\n'+str(self.edges)+'\n'+str(self.faces)+'\n'+str(self.borders)
+        return s
+        '''print(g.vert_e)
+        print(g.edges)
+        print(g.faces)
+        print(g.borders)'''
+        
 
     def add_vertex(self,face): #adds a new vertex inside of face
         self.vert_e.append([-self.borders_num-1])
@@ -58,17 +71,23 @@ class TopologicalGraph():
             v2=self.edges[e2][0]'''
         self.edges.append([v1,v2,-1,-1,-1])
         self.edges.append([v2,v1,-1,-1,-1])
-        print(self.vert_e[v2])
         if len(self.vert_e[v1])==1 and self.vert_e[v1][0]<0:
             b1=-self.vert_e[v1][0]-1     # b1 and b2 are borders
             self.vert_e[v1]=[]
         else:
+            if e1==None:
+                e1=self.vert_e[v1][0]
             b1=self.edges[e1][2]
         if len(self.vert_e[v2])==1 and self.vert_e[v2][0]<0:
             b2=-self.vert_e[v2][0]-1
             self.vert_e[v2]=[]
         else:
-            b2=self.edges[e1][2]
+            if e2==None and len(self.vert_e[v2])>0:
+                e2=self.vert_e[v2][0]
+            if v1==v2:
+                b2=b1
+            else:
+                b2=self.edges[e2][2]
         self.vert_e[v1].append(len(self.edges)-2)
         self.vert_e[v2].append(len(self.edges)-1)
         if b1!=b2:
@@ -80,63 +99,96 @@ class TopologicalGraph():
             del self.faces[k1][b2]
             del self.borders[b2]
             self.borders[b1]=(len(self.edges)-2,k1)
-            l1=self.edges[j1][3]
-            l2=self.edges[j2][3]
             self.edges[len(self.edges)-2][2]=b1
-            self.edges[len(self.edges)-2][3]=l1
-            self.edges[len(self.edges)-2][4]=j2
             self.edges[len(self.edges)-1][2]=b1
-            self.edges[len(self.edges)-1][3]=j1
-            self.edges[len(self.edges)-1][4]=l2
-            self.edges[j1][3]=len(self.edges)-1
-            self.edges[l1][4]=len(self.edges)-2
-            self.edges[j2][3]=len(self.edges)-2
-            self.edges[l2][4]=len(self.edges)-1
-            self.edges[j2][2]=b1
-            while j2!=l2:
-                j2=self.edges[j2][4]
+            if e1!=None:
+                l1=self.edges[j1][3]
+                self.edges[len(self.edges)-2][3]=l1
+                self.edges[len(self.edges)-1][4]=j1
+                self.edges[j1][3]=len(self.edges)-1
+                self.edges[l1][4]=len(self.edges)-2
+            else:
+                self.edges[len(self.edges)-2][3]=len(self.edges)-1
+                self.edges[len(self.edges)-1][4]=len(self.edges)-2
+            if e2!=None:
+                l2=self.edges[j2][3]
+                self.edges[len(self.edges)-2][4]=j2
+                self.edges[len(self.edges)-1][3]=l2
+                self.edges[j2][3]=len(self.edges)-2
+                self.edges[l2][4]=len(self.edges)-1
                 self.edges[j2][2]=b1
+                while j2!=l2:
+                    j2=self.edges[j2][4]
+                    self.edges[j2][2]=b1
+            else:
+                self.edges[len(self.edges)-2][4]=len(self.edges)-1
+                self.edges[len(self.edges)-1][3]=len(self.edges)-2
+                
         else:
             k=self.borders[b1][1]
             self.borders[self.borders_num]=(len(self.edges)-1,len(self.faces))
             self.borders[b1]=(len(self.edges)-2,k)
-            l1=self.edges[e1][3]
-            l2=self.edges[e2][3]
             self.edges[len(self.edges)-2][2]=b1
-            self.edges[len(self.edges)-2][3]=l1
-            self.edges[len(self.edges)-2][4]=e2
             self.edges[len(self.edges)-1][2]=self.borders_num
-            self.edges[len(self.edges)-1][3]=e1
-            self.edges[len(self.edges)-1][4]=l2
-            self.edges[e1][3]=len(self.edges)-1
-            self.edges[l1][4]=len(self.edges)-2
-            self.edges[e2][3]=len(self.edges)-2
-            self.edges[l2][4]=len(self.edges)-1
-            j=e1
-            self.edges[e1][2]=self.borders_num
-            while j!=l2:
-                j=self.edges[j][4]
-                self.edges[j][2]=self.borders_num
+            if e1!=None:
+                l1=self.edges[e1][3]
+                l2=self.edges[e2][3]
+                self.edges[len(self.edges)-2][3]=l1
+                self.edges[len(self.edges)-2][4]=e2
+                self.edges[len(self.edges)-1][3]=e1
+                self.edges[len(self.edges)-1][4]=l2
+                self.edges[e1][3]=len(self.edges)-1
+                self.edges[l1][4]=len(self.edges)-2
+                self.edges[e2][3]=len(self.edges)-2
+                self.edges[l2][4]=len(self.edges)-1
+                j=e1
+                self.edges[e1][2]=self.borders_num
+                while j!=l2:
+                    j=self.edges[j][4]
+                    self.edges[j][2]=self.borders_num
+            else:
+                self.edges[len(self.edges)-2][3]=len(self.edges)-2
+                self.edges[len(self.edges)-2][4]=len(self.edges)-2
+                self.edges[len(self.edges)-1][3]=len(self.edges)-1
+                self.edges[len(self.edges)-1][4]=len(self.edges)-1
             self.faces.append({})
+            self.faces[-1][self.borders_num]=True
             if borders:
-                for i in range (len(borders)): 
+                for i in range (len(borders)):
+                    print(self.faces[-1],borders[i])
                     self.faces[-1][borders[i]]=True
                     del self.faces[k][borders[i]]
-                    self.borders[borders[i]]=(self.borders[borders[i]][0],len(self.faces)-1)       
+                    self.borders[borders[i]]=(self.borders[borders[i]][0],len(self.faces)-1)
+            self.borders_num+=1
 
-
-g=TopologicalGraph(5,[(0,2),(1,2),(0,1),(3,4)],[[[(2,False),(0,True),(1,False)]],[[(2,True),(0,False),(1,True)],[(3,False),(3,True)]]])
-print(g.vert_e)
-print(g.edges)
-print(g.faces)
-print(g.borders)
+g=TopologicalGraph(0,[],[[]])
 g.add_vertex(0)
+g.add_vertex(0)
+g.add_edge(0,1,None,None)
+print(g)
+g.add_vertex(0)
+print(g)
+
+
+
+
+'''g=TopologicalGraph(5,[(0,2),(1,2),(0,1),(3,4)],[[[(2,False),(0,True),(1,False)]],[[(2,True),(0,False),(1,True)],[(3,False),(3,True)]]])
 print(g.vert_e)
 print(g.edges)
 print(g.faces)
 print(g.borders)
-g.add_edge(5,5,[0])
+g.add_edge(0,3,4,6)
+print(g.vert_e)
+print(g.edges)
+print(g.faces)
+print(g.borders)'''
+'''g.add_vertex(0)
 print(g.vert_e)
 print(g.edges)
 print(g.faces)
 print(g.borders)
+g.add_edge(5,5,None,None)
+print(g.vert_e)
+print(g.edges)
+print(g.faces)
+print(g.borders)'''
